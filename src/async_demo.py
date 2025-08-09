97c98bd6bf44986689618aac679d2411b7ba6bc3
altusea <zt.song@outlook.com>
2025-08-09 17:29:15 +0800

    build(deps): 添加 orjson依赖并进行异步测试

--- src/async_demo.py
+++ src/async_demo.py
@@ -1,9 +1,11 @@
 import asyncio
 
+
 async def main():
     print("Hello")
     await asyncio.sleep(5)
     print("World")
+
 
 # 运行协程
 asyncio.run(main())
---
+++ src/serde_test.py
@@ -0,0 +1,9 @@
+from orjson import orjson
+
+from src.utils.model import Point
+
+p1 = Point(1, 2)
+json_str = orjson.dumps(p1)
+print(json_str)
+p2 = orjson.loads(json_str)
+print(p2)
