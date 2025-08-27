af24c3d8dce1a874995e8d21a4bae3ad9e6af8c1
altusea <zt.song@outlook.com>
2025-08-27 16:08:42 +0800

    描述器入门

---
+++ src/learn/discryptor.py
@@ -0,0 +1,106 @@
+import os
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+
+class Ten:
+    def __get__(self, obj, objtype=None):
+        return 10
+
+
+class A:
+    x = 5  # 常规类属性
+    y = Ten()  # 描述器实例
+
+
+class DirectorySize:
+    def __get__(self, obj, objtype=None):
+        return len(os.listdir(obj.dirname))
+
+
+class Directory:
+    size = DirectorySize()  # 描述器实例
+
+    def __init__(self, dirname):
+        self.dirname = dirname  # 常规实例属性
+
+
+class Validator(ABC):
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class OneOf(Validator):
+    def __init__(self, *options):
+        self.options = set(options)
+
+    def validate(self, value):
+        if value not in self.options:
+            raise ValueError(f"Expected {value!r} to be one of {self.options!r}")
+
+
+class Number(Validator):
+    def __init__(self, minvalue=None, maxvalue=None):
+        self.minvalue = minvalue
+        self.maxvalue = maxvalue
+
+    def validate(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Expected {value!r} to be an int or float")
+        if self.minvalue is not None and value < self.minvalue:
+            raise ValueError(f"Expected {value!r} to be at least {self.minvalue!r}")
+        if self.maxvalue is not None and value > self.maxvalue:
+            raise ValueError(f"Expected {value!r} to be no more than {self.maxvalue!r}")
+
+
+class String(Validator):
+    def __init__(self, minsize=None, maxsize=None, predicate=None):
+        self.minsize = minsize
+        self.maxsize = maxsize
+        self.predicate = predicate
+
+    def validate(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f"Expected {value!r} to be an str")
+        if self.minsize is not None and len(value) < self.minsize:
+            raise ValueError(
+                f"Expected {value!r} to be no smaller than {self.minsize!r}"
+            )
+        if self.maxsize is not None and len(value) > self.maxsize:
+            raise ValueError(
+                f"Expected {value!r} to be no bigger than {self.maxsize!r}"
+            )
+        if self.predicate is not None and not self.predicate(value):
+            raise ValueError(f"Expected {self.predicate} to be true for {value!r}")
+
+
+class Component:
+    name = String(minsize=3, maxsize=10, predicate=str.isupper)
+    kind = OneOf("wood", "metal", "plastic")
+    quantity = Number(minvalue=0)
+
+    def __init__(self, name, kind, quantity):
+        self.name = name
+        self.kind = kind
+        self.quantity = quantity
+
+
+if __name__ == "__main__":
+    a = A()
+    print(a.x)  # 输出: 5
+    print(a.y)  # 输出: 10 (通过描述器获取)
+    s = Directory(Path("~/workspace").expanduser())
+    print(s.size)  # 输出: 目录中的文件数量 (通过描述器获取)
+    c = Component('WIDGET', 'metal', 5)
+    print(c.name, c.kind, c.quantity)  # 输出: WIDGET metal 5
