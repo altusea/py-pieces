a63c65019ef7441900c43bb3a91cba382cb17ba6
altusea <zt.song@outlook.com>
2025-10-07 09:14:31 +0800

    build: update dependencies

--- src/anyio_test.py
+++ src/anyio_test.py
@@ -4,8 +4,9 @@ from anyio import sleep
 
 
 async def main():
-    print('Hello')
+    print("Hello")
     await sleep(1)
     print("I'm running on", sniffio.current_async_library())
+
 
 trio.run(main)
